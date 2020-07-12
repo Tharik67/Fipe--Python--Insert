3e404b0c125324e89b83fdd904b982d376c9d06f
Tharik Lourenco <tharik.augusto67@gmail.com>
2020-07-11 21:09:28 -0300

    correcao

--- negocios.py
+++ negocios.py
@@ -16,21 +16,26 @@ def gera_data(comeco , fim):
     date=datetime.date(random.randint(comeco , fim), random.randint(1,12),random.randint(1,28))
     return date
 
+arq_auto = open("automoveis.txt",'r') 
+ntotal = 0  
+for el in arq_auto:
+    ntotal +=1
+arq_auto.close()
 
-
-def get_carro():
+def get_carro(total = ntotal):
     arq_auto = open("automoveis.txt",'r')
-    i = random.randint(1,1042)
+    #numero de carros 
+    i = random.randint(1,total)
     for el in arq_auto:
         i-=1
         if i == 0:
             a =el.split(',')
             aux= a[0].split(' ')
-            b= aux[3][1:]
-            c= int(a[3])
-            d = int(a[5][:-3])
+            b= aux[3][2:-1]
+            ano= int(a[3])
+            preco = int(a[5][:-3])
             arq_auto.close()
-            return (b,c,d)
+            return (b,ano, preco)
     
 
 def get_listcarro(ncar ):
@@ -54,15 +59,17 @@ cnpjs = []
 for linha in arq_cnpj:
     linha.strip()
     cnpjs.append(linha[:-1])
+arq_cnpj.close()
     
 arq_cpf = open('cpf.txt','r')
 cpfs = []
 for linha in arq_cpf:
     linha.strip()
     cpfs.append(linha[:-1])
+arq_cpf.close()
 
 saida_negocios = open("negocios.txt" , "w")  
-for CPF in cpfs:    
+for CPF in cpfs:   
     ncar = random.randint(1,2)
     k = random.randint(1,3)
     revendas = random.choices(cnpjs , k=k)
@@ -70,16 +77,18 @@ for CPF in cpfs:
     for carro in carros:
         cnpj = random.choice(revendas)
         codigo = carro[0]
-        ano = carro[1]
-        data = gera_data(carro[1] , 2020)
-        variacao = random.randint(1,3)
+        ano = int(carro[1])
+        data = gera_data(int(carro[1]) , 2020)
+        variacao = random.randint(1,3)/10
         preco = carro[2] *variacao
 
         txt = "Insert into negocios ('%s' , '%s','%s' , %d ,'%s' , %d); \n" %(CPF , cnpj,codigo ,ano , data, preco)
+        
         saida_negocios.write(txt)
+        
     
     #(CPFcomprador, CNPJrevenda, CodigoAuto, AnoAuto, Data, Preço)  
     
 saida_negocios.close()
-arq_cnpj.close()
-arq_cpf.close()
+
+
